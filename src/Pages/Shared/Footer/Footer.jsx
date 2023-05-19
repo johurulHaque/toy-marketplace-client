440451b9649e209df6fc5402a39db3fa8f9b11a6
johurulhque <johurulhque.cse@gmail.com>
2023-05-19 18:06:14 +0600

    footer is added

--- src/Pages/Shared/Footer/Footer.jsx
+++ src/Pages/Shared/Footer/Footer.jsx
@@ -1,46 +1,69 @@
 import React from "react";
+import logo from "../../../assets/toy.png";
+import {
+  FaPhone,
+  FaMapMarkerAlt,
+  FaMailBulk,
+  FaFacebook,
+  FaInstagram,
+  FaTwitter
+} from "react-icons/fa";
 
 const Footer = () => {
   return (
-    <footer className="footer p-10 bg-base-200 text-base-content">
+    <footer className="footer p-10  bg-[url('/footer.png')] h-[100%] text-base-content">
       <div>
-        <svg
-          width="50"
-          height="50"
-          viewBox="0 0 24 24"
-          xmlns="http://www.w3.org/2000/svg"
-          fillRule="evenodd"
-          clipRule="evenodd"
-          className="fill-current"
-        >
-          <path d="M22.672 15.226l-2.432.811.841 2.515c.33 1.019-.209 2.127-1.23 2.456-1.15.325-2.148-.321-2.463-1.226l-.84-2.518-5.013 1.677.84 2.517c.391 1.203-.434 2.542-1.831 2.542-.88 0-1.601-.564-1.86-1.314l-.842-2.516-2.431.809c-1.135.328-2.145-.317-2.463-1.229-.329-1.018.211-2.127 1.231-2.456l2.432-.809-1.621-4.823-2.432.808c-1.355.384-2.558-.59-2.558-1.839 0-.817.509-1.582 1.327-1.846l2.433-.809-.842-2.515c-.33-1.02.211-2.129 1.232-2.458 1.02-.329 2.13.209 2.461 1.229l.842 2.515 5.011-1.677-.839-2.517c-.403-1.238.484-2.553 1.843-2.553.819 0 1.585.509 1.85 1.326l.841 2.517 2.431-.81c1.02-.33 2.131.211 2.461 1.229.332 1.018-.21 2.126-1.23 2.456l-2.433.809 1.622 4.823 2.433-.809c1.242-.401 2.557.484 2.557 1.838 0 .819-.51 1.583-1.328 1.847m-8.992-6.428l-5.01 1.675 1.619 4.828 5.011-1.674-1.62-4.829z"></path>
-        </svg>
+        <img src={logo} alt="" className="w-36" />
         <p>
-          ACME Industries Ltd.
+          Toys Home Ltd.
           <br />
-          Providing reliable tech since 1992
+          <p>Copyright © 2023 - All right reserved</p>
         </p>
       </div>
       <div>
         <span className="footer-title">Services</span>
-        <a className="link link-hover">Branding</a>
-        <a className="link link-hover">Design</a>
-        <a className="link link-hover">Marketing</a>
-        <a className="link link-hover">Advertisement</a>
+        <a className="link link-hover"> About Us</a>
+        <a className="link link-hover">Contact Us</a>
+        <a className="link link-hover">FAQs</a>
+        <a className="link link-hover">New Products</a>
+        <a className="link link-hover">Best Sales</a>
+      </div>
+
+      <div>
+        <span className="footer-title">PRODUCT</span>
+        <a className="link link-hover">Legal Notice</a>
+        <a className="link link-hover">Delivery</a>
+        <a className="link link-hover">Prices drop</a>
+        <a className="link link-hover">New products</a>
+        <a className="link link-hover">Best sales</a>
       </div>
       <div>
-        <span className="footer-title">Company</span>
-        <a className="link link-hover">About us</a>
-        <a className="link link-hover">Contact</a>
-        <a className="link link-hover">Jobs</a>
-        <a className="link link-hover">Press kit</a>
+        <span className="footer-title">CONTACT INFORMATION</span>
+        <a className="link link-hover flex gap-2">
+          <FaMapMarkerAlt />
+          4005, Silver business point USA
+        </a>
+        <a className="link link-hover flex gap-2">
+          <FaPhone />
+          123 456 789
+        </a>
+        <a className="link link-hover flex gap-2">
+          <FaMailBulk />
+          info@gmail.com
+        </a>
+        <div className="flex gap-3">
+          <a href="https://www.facebook.com/" target="_blank" rel="noreferrer">
+          <FaFacebook className="text-2xl text-[#09ccd0]" />
+          </a>
+          <a href="https://www.instagram.com/" target="_blank" rel="noreferrer">
+          <FaInstagram className="text-2xl text-[#09ccd0]" />
+          </a>
+          <a href="https://twitter.com/" target="_blank" rel="noreferrer">
+          <FaTwitter className="text-2xl text-[#09ccd0]" />
+          </a>
+        </div>
       </div>
-      <div>
-        <span className="footer-title">Legal</span>
-        <a className="link link-hover">Terms of use</a>
-        <a className="link link-hover">Privacy policy</a>
-        <a className="link link-hover">Cookie policy</a>
-      </div>
+      <div></div>
     </footer>
   );
 };
