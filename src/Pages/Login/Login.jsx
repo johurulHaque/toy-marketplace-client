ddf3000855212daa23e5586ffbd60ba694326096
johurulhque <johurulhque.cse@gmail.com>
2023-05-20 13:16:35 +0600

    login page is complete

--- src/Pages/Login/Login.jsx
+++ src/Pages/Login/Login.jsx
@@ -2,13 +2,14 @@ import React, { useContext, useState } from "react";
 import { Link, useLocation, useNavigate } from "react-router-dom";
 import { FaGoogle } from "react-icons/fa";
 import { AuthContext } from "../../Provider/AuthProvider";
+import Lottie from "lottie-react";
+import lottieLogin from "../../assets/lottiefile.json";
+
 const Login = () => {
   const { signIn, googleSign } = useContext(AuthContext);
   const location = useLocation();
   const navigate = useNavigate();
   const from = location?.state?.from?.pathname || "/";
-
-
 
   const [success, setSuccess] = useState("");
   const [error, setError] = useState("");
@@ -47,34 +48,53 @@ const Login = () => {
   };
   return (
     <div className="register-form">
-      <h2 className="text-2xl font-bold mb-4">Please Register here</h2>
-      <form onSubmit={handleLogin}>
-        <div className="mb-4">
-          <input
-            type="email"
-            id="email"
-            name="email"
-            placeholder="Enter your email"
-            className="input input-bordered"
-          />
+      <div className="grid grid-cols-2 gap-4">
+        <div>
+          <form onSubmit={handleLogin}>
+            <div className="mb-4">
+              <input
+                type="email"
+                id="email"
+                name="email"
+                placeholder="Enter your email"
+                className="input input-bordered"
+              />
+            </div>
+            <div className="mb-4">
+              <input
+                type="password"
+                id="password"
+                name="password"
+                placeholder="Enter your password"
+                className="input input-bordered"
+              />
+            </div>
+            <button
+              type="submit"
+              className="bg-[#09ccd0] px-4 py-2 rounded text-white"
+            >
+              Login
+            </button>
+            <Link to="/register">or Register</Link>
+          </form>
+          <span className="text-green-500">{success}</span>
+          <span className="text-red-500">{error}</span>
         </div>
-        <div className="mb-4">
-          <input
-            type="password"
-            id="password"
-            name="password"
-            placeholder="Enter your password"
-            className="input input-bordered"
-          />
-        </div>
-        <button type="submit" className="btn btn-primary">
-          Login
-        </button>
-      </form>
-      <span className="text-green-500">{success}</span>
-      <span className="text-red-500">{error}</span>
-      <button onClick={handleGoogle} className="btn btn-primary">
-        Sing With Google <FaGoogle />
+
+        <Lottie animationData={lottieLogin} loop={true} />
+      </div>
+
+      <button
+        onClick={handleGoogle}
+        className="bg-[#09ccd0] px-6 py-2 rounded text-white"
+      >
+        <span className="flex items-center gap-3">
+          Sing With Google{" "}
+          <span className="text-[#ff6f69] font-bold text-2xl">
+            {" "}
+            <FaGoogle />
+          </span>
+        </span>
       </button>
     </div>
   );
